df26fde1e5700150a941007efce83cb5b3980592
Logicon211 <logicon211@gmail.com>
2017-11-30 20:28:58 -0400

    Committing Assets Part 8

---
+++ LudumDare40/Assets/LudumDare37 Assets/WinGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinGame : MonoBehaviour {
+
+	public int victoryScreenIndex;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag ("Player")) {
+			Application.LoadLevel(victoryScreenIndex);
+		}
+	}
+}
---
+++ LudumDare40/Assets/LudumDare37 Assets/WinLossChecker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WinLossChecker : MonoBehaviour {
+
+	public GameObject player;
+	//public GameObject gameEndingBarrel;
+
+	private PlayerHealth playerHealthScript;
+	//private GameEndingBarrel gameEndingBarrelScript;
+	// Use this for initialization
+	void Start () {
+		playerHealthScript = player.GetComponent<PlayerHealth>();
+		//gameEndingBarrelScript = gameEndingBarrel.GetComponent<GameEndingBarrel>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(player == null || playerHealthScript.getDead()) {
+			StartCoroutine("LoseGame");
+		}
+
+//		if(gameEndingBarrel == null || gameEndingBarrelScript.health <= 0) {
+//			StartCoroutine("WinGame");
+//		}
+	}
+
+	IEnumerator LoseGame() {			
+		yield return new  WaitForSeconds(3);  // or however long you want it to wait
+		Application.LoadLevel("GameOverScreen");
+	}
+
+	IEnumerator WinGame() {			
+		yield return new WaitForSeconds(3);  // or however long you want it to wait
+		Application.LoadLevel("VictoryScreen");
+	}
+}
