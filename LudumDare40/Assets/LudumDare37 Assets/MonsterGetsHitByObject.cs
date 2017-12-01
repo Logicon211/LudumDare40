688c7c3388cf57a345384803185b4750ca3f055b
Logicon211 <logicon211@gmail.com>
2017-11-30 20:23:03 -0400

    Committing Assets Part 4

---
+++ LudumDare40/Assets/LudumDare37 Assets/MonsterGetsHitByObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterGetsHitByObject : MonoBehaviour {
+
+    MonsterHealth health;
+	public GameObject hitEffect;
+
+	// Use this for initialization
+	void Start () {
+        health = gameObject.GetComponent<MonsterHealth>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        GameObject g = other.collider.gameObject;
+        if (g.GetComponent<GenericPickupableObject>() != null)
+        {
+            float vel = other.relativeVelocity.magnitude;
+            float mass = g.GetComponent<Rigidbody>().mass;
+
+            if (mass*vel >= 4.0f)
+            {
+				Instantiate(hitEffect, other.contacts[0].point , Quaternion.identity);
+                health.SetHealth(10);
+            }
+        }
+    }
+}
---
+++ LudumDare40/Assets/LudumDare37 Assets/MonsterHealth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterHealth : MonoBehaviour {
+
+    AudioSource audio;
+
+    public int health;
+    public AudioClip damageSound;
+	//public GameObject hitEffect;
+
+    bool isDead;
+
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        isDead = (health <= 0);
+	}
+
+    public void SetHealth(int damage)
+    {
+		//Instantiate(hitEffect, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        audio.PlayOneShot(damageSound, 1.0f);
+        health -= damage;
+    }
+
+    public bool IsDead()
+    {
+        return isDead;
+    }
+}
