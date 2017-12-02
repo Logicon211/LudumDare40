c9595c965b0e30bad8ecae81f57536ebde4a44a4
KingOfBraves <chadifferus@gmail.com>
2017-12-02 18:29:24 -0400

    Basic spawning references #9

---
+++ LudumDare40/Assets/Scripts/BabySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BabySpawner : MonoBehaviour {
+
+    public GameObject babyFab;
+
+    public float maxTimer = 2f;
+
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+        timer = maxTimer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if (timer <= 0f) {
+            Instantiate(babyFab, transform.position, transform.rotation);
+            timer = maxTimer;
+        }
+	}
+}
