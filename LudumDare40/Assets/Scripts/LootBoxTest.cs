56e763242e7eced367e71f93e7ae68a7a19d333e
KingOfBraves <chadifferus@gmail.com>
2017-12-02 14:30:25 -0400

    references #17 Finished loot box prefab, this is probably not the proper issue references #1 please get him out of here

---
+++ LudumDare40/Assets/Scripts/LootBoxTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootBoxTest : MonoBehaviour {
+
+    public LootBox lootBox;
+
+    private bool isOpen = false;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space")) {
+            if (!isOpen) {
+                Debug.Log("FUCK");
+                lootBox.ActivateLootBox();
+                isOpen = !isOpen;
+            }
+            else {
+                Debug.Log("FUCK");
+                lootBox.DeactivateLootBox();
+                isOpen = !isOpen;
+            }
+        
+        }
+	}
+}
