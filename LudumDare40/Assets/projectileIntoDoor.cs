40ed06c5f179cd07f19b8109d84c324beb4969f7
Logicon211 <logicon211@gmail.com>
2017-12-02 17:54:58 -0400

    New car stuff

--- LudumDare40/Assets/projectileIntoDoor.cs
+++ LudumDare40/Assets/projectileIntoDoor.cs
@@ -6,6 +6,9 @@ public class projectileIntoDoor : MonoBehaviour {
 
 	public Door door;
 
+	public Light spotLight;
+	public Light pointLight;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -23,7 +26,10 @@ public class projectileIntoDoor : MonoBehaviour {
 			//If it's not already closed
 			if (door.State != 1) {
 				if (door.RotationPending == false) StartCoroutine(door.Move());
+
 				//Score some points. make some noise. close the damn door
+				spotLight.enabled = false;
+				pointLight.enabled = false;
 
 				Destroy(other.gameObject);
 			}
