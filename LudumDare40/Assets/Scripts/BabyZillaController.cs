7f0c075bf5d17875727ee6f868963222205a2936
BroderickBeaman <broderick.beaman@unb.ca>
2017-12-02 02:04:37 -0400

    Trying to get babyzilla collisions working

---
+++ LudumDare40/Assets/Scripts/BabyZillaController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BabyZillaController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Hey");
+		if (other.gameObject.CompareTag ("Baby")) {
+			other.gameObject.GetComponent<BabyController> ().SetPartOfBabyZilla (true);
+		}
+	}
+}
