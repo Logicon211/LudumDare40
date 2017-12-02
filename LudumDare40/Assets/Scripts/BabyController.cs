00fa7ff6c0e90c7bd1c18738bc907493b7e4d8d8
BroderickBeaman <broderick.beaman@unb.ca>
2017-12-02 01:31:58 -0400

    Babies mooooove

--- LudumDare40/Assets/Scripts/BabyController.cs
+++ LudumDare40/Assets/Scripts/BabyController.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class BabyController : MonoBehaviour {
 
+	public float speed;
+	public GameObject babyZilla;
+
+
+
 	// Use this for initialization
 	void Start () {
-		
+		babyZilla = GameObject.FindGameObjectWithTag ("Babyzilla");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		float step = speed * Time.deltaTime;
+		this.transform.position = Vector3.MoveTowards (this.transform.position, babyZilla.transform.position, step);
 	}
 }
