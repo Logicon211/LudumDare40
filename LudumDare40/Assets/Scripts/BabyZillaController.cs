cba40145ed1ef83de0f5eb80a8d1958e5533755e
BroderickBeaman <broderick.beaman@unb.ca>
2017-12-02 11:46:43 -0400

    Checkpoint commit for baby scripts

--- LudumDare40/Assets/Scripts/BabyZillaController.cs
+++ LudumDare40/Assets/Scripts/BabyZillaController.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class BabyZillaController : MonoBehaviour {
 
+	private int numberOfBabies = 0;
+	private int maxBabyCapacity = 10;
+	private int[] babiesAtEachLevel;
+
+	private List<Vector3> babyPositions;
+
 	// Use this for initialization
 	void Start () {
-		
+		Init ();
 	}
 	
 	// Update is called once per frame
@@ -15,9 +21,39 @@ public class BabyZillaController : MonoBehaviour {
 	}
 		
 	void OnTriggerEnter(Collider other) {
-		Debug.Log ("Hey");
 		if (other.gameObject.CompareTag ("Baby")) {
-			other.gameObject.GetComponent<BabyController> ().SetPartOfBabyZilla (true);
+			AddBabyToBabyZilla (other.gameObject);
 		}
+	}
+
+	private void Init () {
+		babiesAtEachLevel = new int[] { 0, 0, 0, 0 };
+		babyPositions = new List<Vector3> ();
+		babyPositions.Add (new Vector3 (0f, 0f, 0f));
+		babyPositions.Add (new Vector3 (1f, 0f, 0f));
+		babyPositions.Add (new Vector3 (-1f, 0f, 0f));
+		babyPositions.Add (new Vector3 (0f, 0f, 1f));
+		babyPositions.Add (new Vector3 (0f, 0f, -1f));
+	}
+
+	private void AddBabyToBabyZilla (GameObject baby) {
+		baby.GetComponent<BabyController> ().SetPartOfBabyZilla (true);
+		int currentLayer = 0;
+		while (numberOfBabies < maxBabyCapacity && currentLayer < (babiesAtEachLevel.Length - 1) && babiesAtEachLevel [currentLayer] > babiesAtEachLevel [currentLayer + 1]) {
+			currentLayer++;
+		}
+
+		if (numberOfBabies < maxBabyCapacity) {
+			MakeBabyChildOfBabyZilla (baby, currentLayer);
+		} else {
+//			Some lose condition
+		}
+	}
+
+	private void MakeBabyChildOfBabyZilla (GameObject baby, int layer) {
+		Vector3 testPosition = babyPositions [babiesAtEachLevel [layer]];
+		testPosition.y = layer;
+		Debug.Log (testPosition);
+		babiesAtEachLevel [layer]++
 	}
 }
