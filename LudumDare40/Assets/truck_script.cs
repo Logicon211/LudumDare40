76758a9e9666c4a1eb6d4c99d78fc3e060c10bca
Mitchell MacLean <heavyiam@hotmail.com>
2017-12-03 13:54:07 -0400

    Ever just sit in your truck

---
+++ LudumDare40/Assets/truck_script.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class truck_script : MonoBehaviour {
+
+	Rigidbody rb;
+
+	public float speed = 10f;
+	public float m_GroundCheckDistance = 0.1f;
+
+	private bool m_IsGrounded = false;
+	bool timeToDie = false;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		StartCoroutine ("DieTime");
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hitInfo;
+
+		if (Physics.Raycast (transform.position + (Vector3.up * 0.1f), -transform.up, out hitInfo, m_GroundCheckDistance)) {
+			m_IsGrounded = true;
+			// rb.velocity = Vector3.forward * speed;
+			
+			rb.velocity = transform.forward * speed; //Quaternion.AngleAxis(transform.eulerAngles.y, Vector3.up) * (Vector3.forward * speed);
+		
+					
+			if (timeToDie) {
+				transform.rotation = Quaternion.Slerp (
+					transform.rotation,
+					Quaternion.LookRotation (-transform.right),
+					Time.deltaTime * 1f
+				);
+			}
+				
+		}
+	}
+
+
+
+	IEnumerator DieTime()
+	{
+		print("CHARGING");
+		yield return new WaitForSeconds(2f);
+		timeToDie = true;
+		print("DONE CHARGING");
+
+
+	}
+}
