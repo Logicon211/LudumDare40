ab7d242c776b37021bd5eb71aec9d79929a3c7d5
Mitchell MacLean <heavyiam@hotmail.com>
2017-12-03 20:58:36 -0400

    game controller updates, new Lose
    car spawner, reduce frequency
    hud canvas, add game over image

--- LudumDare40/Assets/Scripts/GameController.cs
+++ LudumDare40/Assets/Scripts/GameController.cs
@@ -2,21 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
 	public int numberOfBabiesToWin;
 
 	private int currentNumberOfBabies = 0;
+	public BabyOverlord BOverlord;
+
+	public Camera mainCamera;
+	public Camera LoseCam;
+	public Image gameOverImage;
+	public Image gameOverImage2;
+	public bool fadeGameOver;
+	private Color gameoverColor;
+	private int fadePercent;
+	public CarSpawner[] carSpawners;
 
 	// Use this for initialization
 	void Start () {
+		LoseCam.enabled = false;
+		gameoverColor = gameOverImage.material.color;
+		gameoverColor.a = 0;
+		gameOverImage.color = gameoverColor;
+		gameOverImage2.color = gameoverColor;
+		fadeGameOver = false;
+		fadePercent = 0;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (fadeGameOver) {
+			fadePercent++;
+			Mathf.Clamp (fadePercent, 0, 100);
+			gameoverColor.a = fadePercent;
+			gameOverImage.color = gameoverColor;
+			gameOverImage2.color = gameoverColor;
+			if (fadePercent == 100) {
+				killAll ();
+			}
+
+		}
 	}
 
 	public void IncrementNumberOfBabies () {
@@ -33,12 +62,45 @@ public class GameController : MonoBehaviour {
 	}
 
 	public void Lose () {
+		
 		//Something happens when you lose
 
 		//Wait a bit before instantly losing?
 
-		SceneManager.LoadScene(4);
+
+		mainCamera.enabled = false;
+		LoseCam.enabled = true;
+		BOverlord.ActivateArmageddon ();
+
+
+		StartCoroutine ("loseScreenDelay");
+		//SceneManager.LoadScene(4);
 		//LoadingScreenManager.LoadScene(4);
 		Debug.Log ("You Lose");
+	}
+
+	public void killAll(){
+
+		GameObject[] gos = GameObject.FindGameObjectsWithTag("Baby");
+		foreach(GameObject go in gos)
+			Destroy(go);
+
+		GameObject[] gos2 = GameObject.FindGameObjectsWithTag("Car");
+		foreach(GameObject go in gos2)
+			Destroy(go);
+
+		BOverlord.gameObject.SetActive (false);
+
+		foreach (CarSpawner go in carSpawners)
+			go.gameObject.SetActive (false);
+
+	}
+
+	IEnumerator loseScreenDelay()
+	{
+
+		yield return new WaitForSeconds(5f);
+		fadeGameOver = true;
+
 	}
 }
